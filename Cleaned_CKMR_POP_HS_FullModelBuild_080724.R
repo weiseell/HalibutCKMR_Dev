adb51ec88e927fef72984fb26ad9534c644eacc7
weiseell <weise.ellie97@gmail.com>
2024-08-07 13:34:45 -0300

    added a clean(er) copy of full model as is

--- Cleaned_CKMR_POP_HS_FullModelBuild_080724.R
+++ Cleaned_CKMR_POP_HS_FullModelBuild_080724.R
@@ -16,6 +16,9 @@ load("Inputs/haps_HSs_Sim_080124.rda")
 source("ModelFunction/prob_la.R")
 source("ModelFunction/growthFunGen.R")
 
+####
+## dat inputs for the modle
+####
 Narray = array(CS_181_195$abundance,c(50,15,2))
 dat <- list()
 dat$Narray <- Narray
@@ -59,8 +62,8 @@ raw_la_sd_SA <- dat$la_key %>%
   spread(AgeClass,sd) %>% 
   dplyr::select(-sex)
 
-dat$la_means_SA <- offarray(as.matrix(raw_la_means_SA),dimseq=list( s=dat$SEXES, a=dat$A))
-dat$la_sd_SA <- offarray(as.matrix(raw_la_sd_SA),dimseq=list( s=dat$SEXES, a=dat$A))
+dat$la_means_SA <- offarray(as.matrix(raw_la_means_SA),dimseq=list(s=dat$SEXES, a=dat$A))
+dat$la_sd_SA <- offarray(as.matrix(raw_la_sd_SA),dimseq=list(s=dat$SEXES, a=dat$A))
 
 # create prob len at age array
 raw_prob_len_at_age <- prob_la(Lmax = max(dat$Lvec),
@@ -74,42 +77,42 @@ dat$prob_len_at_age <- offarray(raw_prob_len_at_age,
 
 ### Fecundity function fix
 #defining shape and scale parameters for pgamma in the
-#fecundity things
+#fecundity functions
 dat <- within( dat, {
   la_shape_SA <- (la_means_SA/la_sd_SA)^2
   la_scale_SA <- la_means_SA/la_shape_SA
 })
 
+####
 ## create parameter list
+####
 parm <- list()
 #log recruitment - random effect length popdynyears
 parm$rw_log_rec <- Narray[1,,1] * 0
 parm$rw_log_rec_sd = log(0.3)
 parm$noise_logrec_dev <- Narray[1,,1] * 0
 parm$noise_rec_dev_sd_log = log(0.3)
-#parm$log_rec <- rep_len(x = 0.1, length.out = dat$Y)
 
 parm$log_rec <- c(10,10)
 #total log abundance - split into Y/A abundance within the model
-#parm$log_init_abundance <- 0.1
 parm$log_init_abundance <- log(sum(Narray[-1,1,]))
 # average log survival for M and F
 parm$log_Z <- log(c(0.2,0.2))
 #a and b values for the fecundity equations
-#!# fix these for now
 parm$log_bexp <- c(0,0)
 parm$log_lucky_litter_par <- 0
 
+#geometric slope of recruitment
 parm$logslopea0 <- log(0.2)
+#sex ratio
 parm$ppn_female <- 0.5
 
+## function input to RTMB
 new_f <- function(parm) reclasso( by=parm, {
   getAll(dat,parm)
   
-  #exp the sd
+  #exp logged parameter values
   rw_rec_sd = exp(rw_log_rec_sd)
-  
-  ##An array for male/female fecundity
   bexp <- exp(log_bexp)
   rec <- exp(log_rec)
   noise_rec_dev_sd <- exp(noise_rec_dev_sd_log)
@@ -117,19 +120,12 @@ new_f <- function(parm) reclasso( by=parm, {
   slopea0 <- exp(logslopea0)
   
   ##Array for average fecundity by sex and age
+  # centered around 150cm
   make_fecundity <- function(s,len){
     (len/150)^bexp[s]
-    # ((len/150) * (len/150)) ^ (bexp[s]/2) # thought this might work for now, but apparently not
-    # ((len/150)+1)^bexp[s]
   }
   
-  ## create prob_by_length array and then fecun array
-  
-  #prob_len_at_age <- autoloop(
-  #  s=SEXES, lc=LENGTH_CLASSES, a=AGES,
-  #  pnorm( ) - pnorm( )
-  #)
-  
+  ## create fecundity arrays
   fec_sa <- autoloop(
     s=SEXES, a=A, 
     SUMOVER=list(lc=LENGTH_CLASSES), {
@@ -139,7 +135,6 @@ new_f <- function(parm) reclasso( by=parm, {
   fec_sa_quant <- autoloop(
     s=SEXES, a=A, qq=(1:nquant), {
       #calculating length for quantile at mean/sd
-      #l_q <- qnorm(qq/(nquant + 1),mean = la_means_SA[s,a],sd = la_sd_SA[s,a])
       l_q <- qgamma(qq/(nquant + 1),
                     shape = la_shape_SA[s,a],
                     scale = la_scale_SA[s,a])
@@ -147,101 +142,93 @@ new_f <- function(parm) reclasso( by=parm, {
       #make fecundity for length given prob
       make_fecundity(s,l_q) * (1/nquant)
     })
-  
-  ##Plus group for later
+
+  # initialize total abundance array
   N = offarray(0,dimseq = list(SEXES=SEXES,POPY=POPY,AGE=A))
   
+  # initialize nll
   nll = 0
-  ##Random walk for recruitment
-  #for(y in 2:length(POPY)){
-  #  nll = nll - dnorm(log_rec[y],log_rec[y-1],rec_sd,TRUE)
-  #}
   
+  # random walk with noise for recruitment
   nll <- sum(dnorm(diff(rw_log_rec), mean=0, sd=rw_rec_sd))
-  
   nll <- sum(dnorm(noise_logrec_dev, mean=0, sd=noise_rec_dev_sd))
-
+  
+  # cumulating recruitment
   cumul_rw_logrec <- cumsum(rw_log_rec)
-
+  
   #per year change in recruitment
   rec_mul <- exp(cumul_rw_logrec + noise_logrec_dev) 
   
-  ##Put in recruitment
+  ##Put in recruitment into N
   for(s in 1:2){
     N[s,,2] = rec[s] * rec_mul
   }
   
+  # initizalize mortality vector
   Z = 0 * N
+  
   ##Male Mortality
   Z[1,,] = exp(log_Z[1])
   ##Female
   Z[2,,] = exp(log_Z[2])
   
   ## calculating the first year for all ages
+  # initialize
   Ny0 <- offarray(0, dimseq = list(AGE=A))
   Ny0[min(A)] <- 1
   
+  # add the rest of the years using slope a0
   for (a in (min(A)+1):max(A)) {
     Ny0[a] <- Ny0[a-1] * slopea0
   }
   
+  # adjust the plus group
   Ny0[max(A)] <- Ny0[max(A)]/(1-slopea0)
   
+  # adjust the whole vector by total initial abundance
   Ny0 <- Ny0 * exp(log_init_abundance)
   
+  # load vector into N
+  # adjust male/female based on sex ratio parameter
   N[female,min(POPY),] <- Ny0 * ppn_female
   N[male,min(POPY),] <- Ny0 * (1-ppn_female)
   
-  #calculating sex-separated survival
-  #cumsurv_m = rev(cumsum(Z[SLICE=1,SLICE=1,head(A,-1)])) 
-  #cumsurv_f = rev(cumsum(Z[SLICE=2,SLICE=1,head(A,-1)]))
-  #total_surv = sum(cumsurv_m+cumsurv_f)
-  
-  ##rough off-array fix for initial abundance
-  #tmp <- (cumsurv_f/total_surv)*exp(log_init_abundance)
-  #tmp <- c( N[SLICE=2,SLICE=1,SLICE=2], tmp)
-  #N[2,1,] <- tmp 
-  
-  #tmp <- (cumsurv_m/total_surv)*exp(log_init_abundance)
-  #tmp <- c( N[SLICE=1,SLICE=1,SLICE=2], tmp)
-  #N[1,1,] <- tmp
-  
-  #nice adding of initial abundance
-  #N[1,1,A[-1]] <- (cumsurv_m/total_surv)*exp(log_init_abundance)
-  #N[2,1,A[-1]] <- (cumsurv_f/total_surv)*exp(log_init_abundance)
-  
-  ## add the plus group to year 1
+  ## initialize mean plus group age
   Abar_plus <- offarray(0,dimseq = list(SEXES=SEXES,POPY=POPY))
-  
   Abar_plus[,1] <- max(A) + 1/(1-exp(-Z[,SLICE=1,SLICE=max(A)]))
   
+  #set adults as years without recruitment
   adults <- A[-1]
+  
+  # loop to fill in the rest of N
   for (y in 2:length(POPY)) {
-    ## run abundance for the next year
+    ## run abundance for the next year based on mortality
     N[,y,adults] <- autoloop(s = SEXES, a = adults,{
       N[s,y-1,a-1]*exp(-Z[s,y-1,a-1])
     })
     
-    #add the plus group
+    #adjust the plus group
     N[,y,max(A)] <- N[,SLICE=y,SLICE=max(A)] + N[,SLICE=y-1,SLICE=max(A)] * exp(-Z[,SLICE=y-1,SLICE=max(A)])
     
-    #add the Abar equation for the plus group
+    #add the Abar for this year based on the plus group
     Abar_plus[,y] <- c(((Abar_plus[,SLICE=y-1]+1) * N[,SLICE=y-1,SLICE=max(A)] + 
                           max(A) * N[,SLICE=y-1,SLICE=max(A)-1])/
                          (N[,SLICE=y-1,SLICE=max(A)] + N[,SLICE=y-1,SLICE=max(A)-1]))
     
   }
   
-  ## calculate survival array to deal with plus group
-  # used in half-sib probability
+  ## calculate survival array to deal with plus group in probabilities
+  ## used in half-sib probability
+  # make a new max age vector for plus group
   maxage <- max(A) + max(POPY)
   superAge <- 2:maxage
   
-  ## run abundance for the next year
+  ## run abundance for plus group years
+  # make extra years offarray
   superN <- offarray(0,dimseq = list(SEXES=SEXES,POPY=POPY,AGE=superAge))
   superN[,,A] <- N
-  
   superN[,2:max(POPY),max(A)] <- 0
+  # add N for those extra years
   for (y in 2:length(POPY)) {
     # needed to vectorize Z and superN slices to multiply them
     superN[,SLICE=y,max(A):max(maxage)] <- c(superN[,SLICE=(y-1),(max(A):max(maxage))-1]) * 
@@ -264,20 +251,22 @@ new_f <- function(parm) reclasso( by=parm, {
   
   #making the reciprocal because RTMB hates dividing stuff
   recip_TRO_SY <- 1/TRO_SY
-  
+  ####
   ## POP nll using CKMR data ###
+  ####
   # generating the POP probabilities
   # This does all ages for IDEAL measurement
   # autoloop() both CREATES the array and FILLS IT IN
   # looping over all the "equals" at the start
-  # This does the plus-group wrong. We will fix immediately afterwards...
+  # This does the plus-group wrong. We will fix in the next section
   Pr_POP_SYLAB <- autoloop( 
     s1=SEXES, y1=SAMPY, lc1=LENGTH_CLASSES, a1=A,
     b2=POPY, {
-      #sd that parent length is currently from the mean
+      # length of parent
       l1 <- Lvec[lc1]
-      #sd1 <- (l1 - la_means_SA[s1,a1])/la_sd_SA[s1,a1]
+      # quantile of parent at a1 given l1 length
       qq1 <- pgamma(l1,shape = la_shape_SA[s1,a1],scale = la_scale_SA[s1,a1])
+      
       #age of parent when off is born
       a1_at_B2 <- a1 - (y1 - b2)
       
@@ -285,82 +274,77 @@ new_f <- function(parm) reclasso( by=parm, {
       l1_at_B2 <- qgamma(qq1,shape = la_shape_SA[s1,a1_at_B2 |> clamp(A)],
                          scale = la_scale_SA[s1,a1_at_B2 |> clamp(A)])
       
-      
-      
-      #l1_at_B2 <- la_means_SA[s1,a1_at_B2 |> clamp( A)] + 
-      #  sd1 * la_sd_SA[s1,a1_at_B2 |> clamp( A)]
-      #l1_at_B2 <- ifelse( l1_at_B2<0, 0, l1_at_B2) # avoid min, max, etc; diffable (?)
-      # ... preferable to do ifelse() *before* make_fecundity(), so we avoid neg nums to pwr...
-      
-      #!# switch fecundity input from age-based to length-based in the fecundity
-      Prob <- 
-        (y1 >= b2) * # otherwise Molly was dead before Dolly born
+      # generate probability given fecundity of par
+      # and TRO at the offspring brith year
+      Prob <- (y1 >= b2) * # otherwise Molly was dead before Dolly born
         (a1_at_B2 >= 2) *
         make_fecundity(s1,l1_at_B2) * recip_TRO_SY[s1,b2]
       
       Prob
     })
   
-  
   # PLUS GROUP FIXUP HERE
   # adjusting probabilities to account for plus group
   # age of parent is 30+ instead of only 30
-  
   Pr_POP_SYLAB_plus <- autoloop(s1=SEXES, y1=SAMPY, lc1=LENGTH_CLASSES,
                                 b2=POPY, SUMOVER=list(q=1:nquant), {
-    ##sd that parent length is currently from the mean
-    # calculate what a1 is based on the length
-    a1 <- qexp(q/(nquant + 1)) * (Abar_plus[s1,y1] - max(A)) + max(A)
+                                  ##sd that parent length is currently from the mean
+                                  # calculate what a1 is based on the length
+                                  a1 <- qexp(q/(nquant + 1)) * (Abar_plus[s1,y1] - max(A)) + max(A)
+                                  
+                                  l1 <- Lvec[lc1]
+                                  #!# CAN FIX THIS IN THE FUTURE BY CHANGING THE MEANS/SD FOR LENGTH/AGE
+                                  # TO A FUNCTION BASED ON LA DATA
+                                  sd1 <- (l1 - len_at_age1(a1))/sd_at_len1(a1)
+                                  
+                                  #age of parent when off is born
+                                  a1_at_B2 <- a1 - (y1 - b2)
+                                  l1_at_B2 <- len_at_age1(a1_at_B2) + 
+                                    sd1 * sd_at_len1(a1_at_B2)
+                                  
+                                  #probability based on par fecundity
+                                  # and TRO at off birth
+                                  Prob <- (y1 >= b2) * # otherwise Molly was dead before Dolly born
+                                    (a1_at_B2 >= 2) * 
+                                    (l1_at_B2 > 0) * # otherwise length shows that Molly wouldn't have been born yet
+                                    make_fecundity(s1,l1_at_B2) * recip_TRO_SY[s1,b2]
+                                  
+                                  Prob
+                                })
   
-    l1 <- Lvec[lc1]
-    #!# CAN FIX THIS IN THE FUTURE BY CHANGING THE MEANS/SD FOR LENGTH/AGE
-    # TO A FUNCTION BASED ON LA DATA
-    sd1 <- (l1 - len_at_age1(a1))/sd_at_len1(a1)
+  # switching plus group prob
+  Pr_POP_SYLAB[,,,max(A),] <- Pr_POP_SYLAB_plus
   
-    #age of parent when off is born
-    a1_at_B2 <- a1 - (y1 - b2)
-  
-    #length of parent when offspring is born
-    #l1_at_B2 <- qgamma(qq1,shape = la_shape_SA_fun(s1,a1_at_B2),
-    #                   scale = la_scale_SA_fun(s1,a1_at_B2))
-  
-    l1_at_B2 <- len_at_age1(a1_at_B2) + 
-        sd1 * sd_at_len1(a1_at_B2)
-
-    #!# switch fecundity input from age-based to length-based in the fecundity
-    Prob <- (y1 >= b2) * # otherwise Molly was dead before Dolly born
-      (a1_at_B2 >= 2) * 
-      (l1_at_B2 > 0) * # otherwise length shows that Molly wouldn't have been born yet
-      make_fecundity(s1,l1_at_B2) * recip_TRO_SY[s1,b2]
-  
-    Prob
-  })
-  
-  Pr_POP_SYLAB[,,,max(A),] <- Pr_POP_SYLAB[,,,SLICE=max(A),] + Pr_POP_SYLAB_plus
-  
+  # calculating the numerator of the Pr_A 
   num_Pr_A_SYL <- autoloop(
     a=A, s=SEXES, y=SAMPY, lc=LENGTH_CLASSES,
     prob_len_at_age[s,lc,a] * N[s,y,a]
   )              
-  
-  #denom_SYL <- sumover(num_Pr_A_SYL,'a')
+
+  # generate the reciporacle denom to avoid dividing arrays
   recip_denom_SYL <- 1 / autoloop( 
     indices=dimseq( num_Pr_A_SYL)[-1],   # see num_Pr_A_SYL above for actual list
-    SUMOVER=dimseq( num_Pr_A_SYL)[1],    # just the "a"
-    num_Pr_A_SYL[a, s, y, lc]
+    SUMOVER=dimseq( num_Pr_A_SYL)[1], {
+      num_Pr_A_SYL[a, s, y, lc]
+    }   # just the "a"
+   
   )
   
+  # calculating the probability
   Pr_A_SYL <- autoloop(
-    a=A, s=SEXES, y=SAMPY, lc=LENGTH_CLASSES,
-    num_Pr_A_SYL[a, s, y, lc] * recip_denom_SYL[s, y, lc]
+    a=A, s=SEXES, y=SAMPY, lc=LENGTH_CLASSES, {
+      num_Pr_A_SYL[a, s, y, lc] * recip_denom_SYL[s, y, lc]
+    }
   )
   
-  
+  # generating pop prob summed over ages
+  # using the SYL prob
   Pr_POP_SYLB <- autoloop(s1=SEXES, y1=SAMPY, lc1=LENGTH_CLASSES,b2=POPY,
                           SUMOVER = list(a1 = A), {
                             Pr_POP_SYLAB[s1,y1,lc1,a1,b2] * Pr_A_SYL[a1,s1,y1,lc1]
                           })  
   
+  # blurring POP prob over all age possibilities
   Pr_POP_SYLSYL1 <- autoloop(
     s1=SEXES, y1=SAMPY, lc1=LENGTH_CLASSES, s2=SEXES, y2=SAMPY, lc2=LENGTH_CLASSES,
     SUMOVER=list(a1= A, a2= A), 
@@ -372,6 +356,7 @@ new_f <- function(parm) reclasso( by=parm, {
         Pr_A_SYL[ a2, s2, y2, lc2]
     })
   
+  # flipping array for the cases when indiv 2 is the actual parent
   Pr_POP_SYLSYL2 <- autoloop(
     s1=SEXES, y1=SAMPY, lc1=LENGTH_CLASSES, s2=SEXES, y2=SAMPY, lc2=LENGTH_CLASSES, {
       Pr_POP_SYLSYL1[s2,y2,lc2,s1,y1,lc1]
@@ -396,12 +381,11 @@ new_f <- function(parm) reclasso( by=parm, {
         ## prob that pp was father of B1
         fec_sa_quant[female,app,qq] * recip_TRO_SY[female,b1] *
         ## prob of parent surviving from B1 to B2
-        #!# turned this off cause superN is still broken
         Pr_Surv_SYAY[female,b1,app,b2] *
         N[male,b2,(app + (b2-b1)) |> clamp(A)]/N[male,b1,app] *
         ## prob that pp was the father of B2
         fec_sa_quant[female,(app + (b2-b1)) |> clamp(A),qq] * recip_TRO_SY[female,b2] *
-      ## lucky litter factor - same cohort HS problem
+        ## lucky litter factor - same cohort HS problem
         ifelse(b2==b1,
                lucky_litter_par,1)
     }
@@ -433,12 +417,11 @@ new_f <- function(parm) reclasso( by=parm, {
       ## prob that pp was father of B1
       fec_sa_quant[male,app,qq] * recip_TRO_SY[male,b1] *
         ## prob of parent surviving from B1 to B2
-        #!# turned this off because superN is still broken
         Pr_Surv_SYAY[male,b1,app,b2] *
         N[male,b2,(app + (b2-b1)) |> clamp(A)]/N[male,b1,app] *
         ## prob that pp was the father of B2
         fec_sa_quant[male,(app + (b2-b1)) |> clamp(A),qq] * recip_TRO_SY[male,b2] *
-      ## lucky litter factor - same cohort HS problem
+        ## lucky litter factor - same cohort HS problem
         ifelse(b2==b1,
                lucky_litter_par,1)
     }
@@ -537,7 +520,9 @@ new_f <- function(parm) reclasso( by=parm, {
   ## summing likelihoods for all cases
   nll <- nll - sum(dpois(c(N_TKP_SYLSYL),c(E_N_TKP_SYLSYL),log = T),na.rm = T)
   
-  #!# Add mitoDNA HERE
+  ####
+  ### Add mitoDNA for half-sibling groups
+  ####
   for (i in 1:nrow(TKPairs)) {
     #grabbing values fron the mtDNA data input
     TKP_s1 <- TKPairs$s1[i]
@@ -618,7 +603,4 @@ sdr <- sdreport(testo)
 
 pl <- as.list(sdr, "Est")
 plsd <- as.list(sdr, "Std")
-
-
-
 
